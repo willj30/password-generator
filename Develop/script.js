ed0c9c2b5a7f997be936aadf6600406039fa0cb0
willj30 <willjaramillo30@gmail.com>
2022-11-11 01:21:04 -0600

    Changed js to manage prompts instead of checkboxes, updated README and added screenshot, modified CSS a little

--- Develop/script.js
+++ Develop/script.js
@@ -1,81 +1,75 @@
 // Assignment code here
 
-  
-var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-generateBtn.addEventListener("click", writePassword);
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "<", ">", "?", "/", ".", "~"];
 
-  const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  const numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  const special= ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "<", ">", "?", "/", ".", "~"];
- 
+var checkLength = '';
+var checkLower;
+var checkUpper;
+var checkNumber;
+var checkSpecial;
 
-
-  function randomLower() {
-   return lower[Math.floor(Math.random() * lower.length)];
+function generatePassword() {
+  var checkLength = (prompt("How many characters do you need in your password? (Between 8 - 128)"));
+  while(checkLength <=7 || checkLength >= 129)  {
+    alert("Pick a number between 8 and 128, silly!");
+    var checkLength = (prompt("How many characters do you need in your password? (Between 8 - 128 Please)"));
   }
-  function randomUpper() {
-   return upper[Math.floor(Math.random() * upper.length)];
-  }
-  
-  function randomNumeric() {
-    return numeric[Math.floor(Math.random() * numeric.length)];
-  }
-  function randomSpecial() {
-    return special[Math.floor(Math.random() * special.length)];
+  var checkLower = confirm("Do you need lowercase letters?"); 
+  var checkUpper = confirm("Do you need UPPERCASE letters?"); 
+  var checkNumber = confirm("Do you need numbers?"); 
+  var checkSpecial = confirm("Do you need special characters?"); 
+   if (checkLower === false &&
+     checkUpper === false &&
+     checkNumber === false &&
+     checkSpecial === false) {
+    alert("You need to choose at LEAST one option!");
+    var checkLower = confirm("Do you need lowercase letters?"); 
+    var checkUpper = confirm("Do you need UPPERCASE letters?"); 
+    var checkNumber = confirm("Do you need numbers?"); 
+    var checkSpecial = confirm("Do you need special characters?"); 
+    if (checkLower === false &&
+      checkUpper === false &&
+      checkNumber === false &&
+      checkSpecial === false) {
+     alert("You really don't want to pick an option, huh?");
+     var checkLower = confirm("Do you need lowercase letters?"); 
+     var checkUpper = confirm("Do you need UPPERCASE letters?"); 
+     var checkNumber = confirm("Do you need numbers?"); 
+     var checkSpecial = confirm("Do you need special characters?"); 
+    
+     }
+
+  var pwdCharacters = [];
+
+if (checkLower) {
+  pwdCharacters = pwdCharacters.concat(lower)
+}
+if (checkUpper) {
+  pwdCharacters = pwdCharacters.concat(upper)
+}
+if (checkNumber) {
+  pwdCharacters = pwdCharacters.concat(numeric)
+}
+if (checkSpecial) {
+  pwdCharacters = pwdCharacters.concat(special)
 }
 
-const pwdLength = document.querySelector('#characters').value;
+var randomPwd = '';
 
-var lowerBox = document.getElementById('lower').checked;
-
-var upperBox = document.getElementById('upper').checked;
-
-var numericBox = document.getElementById('numeric').checked;
-
-var specialBox = document.getElementById('special').checked;
-
-
-
-function writePassword() {
-  
-  let passwordText = '';
-  for (let i = 0; i < pwdLength; i++) {
-    if(lowerBox && passwordText < pwdLength); {
-     randomLower();
-      passwordText += passwordText + [randomLower()];
-    }
-    if(upperBox && passwordText.length < pwdLength); {
-      randomUpper();
-      passwordText += passwordText + [randomUpper()];
-    }
-    if(numericBox && passwordText.length < pwdLength); {
-      randomNumeric();
-      passwordText += passwordText + [randomNumeric()];
-    }
-   if(specialBox && passwordText.length < pwdLength); {
-      randomSpecial();
-      passwordText += passwordText + [randomSpecial()];
-    }
-    return passwordText;
+for (var i = 0; i < checkLength; i++) {
+randomPwd = randomPwd + pwdCharacters[Math.floor(Math.random() * pwdCharacters.length)];
   }
- 
+  return randomPwd;
   }
 
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
- console.log(writePassword())
-
-
-  // var passwordText = document.querySelector("#password");
-
-
-// function generatePassword () 
-
-
-
-
-  
-//  var password = generatePassword()
-//   var passwordText = document.querySelector("#password");
-// var characters = document.getElementById("characters");
+    passwordText.value = password;
+  }
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Password Generator Starter Code
+# Will's Password Generator
+
+## Description
+
+Here is my very first password generator. I would have finished it on-time but I thought I could make checkboxes work until I realized they wouldn't meet the criteria. In this project I got really intimate with:
+
+- JavaScript
+- Prompts
+- For loops
+- Variables
+- Arrays
+- and my Computer space
+
+With this password generator you will be able to create passwords between 8 and 128 characters with lower case letters, upper case letters, numbers and special characters.
+
+## Installation
+
+All you need is a device with a browswer, a screen, and internet access.
+
+## Usage
+
+The website can be found at this URL: https://willj30.github.io/password-generator/
+
+Website Screenshot:
+
+ ![willj30-Module-1-Challenge](./Develop/images/_C__Users_willj_Desktop_bootcamp_challenges_password-generator_Develop_index.html%20(1).png)  
+
+## Credits
+
+https://www.w3schools.com/
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+https://stackoverflow.com/
+
+## License
+
+MIT License
+
+---
